0bfe116f6dfb2b2447e5470502c3cdf6460b4176
filipandersson3 <60388955+filipandersson3@users.noreply.github.com>
2021-09-02 13:12:41 +0200

    boll kan studsa i x och y led med studskoefficient

--- src/BollSimulation/Main.java
+++ src/BollSimulation/Main.java
@@ -5,20 +5,24 @@ import java.util.concurrent.TimeUnit;
 public class Main {
 
     public static void main(String[] args) {
-	    //int bally = 5;
-        //double ballacc = 9.82;
-        double time = 0;
-        double lasttime = 0;
-        while(true) {
-            time = System.currentTimeMillis()-lasttime;
-            //double ballv = ballacc*time;
-            try {
-                TimeUnit.MILLISECONDS.sleep(500);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
+        double ballx = 0;
+	    double bally = 5;
+        double ballv = 0;
+        double ballvx = 0.1;
+        double ballacc = -9.82;
+        double deltaTime = 0.1;
+        for (double time = 0; time < 5; time+=deltaTime) {
+            if (bally < 0) {
+                ballv = -ballv*0.9;
+                bally = 0;
             }
-            lasttime = time;
-            System.out.println(time);
+            if (ballx > 3) {
+                ballvx = -ballvx;
+            }
+            ballv += ballacc*deltaTime;
+            bally += ballv*deltaTime;
+            ballx += ballvx;
+            System.out.println(bally + " ");
         }
     }
 }
