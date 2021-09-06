9377f18872071f23c59572195a27ce3842a902db
filipandersson3 <60388955+filipandersson3@users.noreply.github.com>
2021-09-06 09:12:19 +0200

    boll studsar som i en backe tror jag (kan inte avgöra)

--- src/BollSimulation/Main.java
+++ src/BollSimulation/Main.java
@@ -8,21 +8,22 @@ public class Main {
         double ballx = 0;
 	    double bally = 5;
         double ballv = 0;
-        double ballvx = 0.1;
+        double ballvx = 0;
         double ballacc = -9.82;
         double deltaTime = 0.1;
-        for (double time = 0; time < 5; time+=deltaTime) {
-            if (bally < 0) {
-                ballv = -ballv*0.9;
-                bally = 0;
-            }
+        for (double time = 0; time < 2; time+=deltaTime) {
             if (ballx > 3) {
                 ballvx = -ballvx;
             }
             ballv += ballacc*deltaTime;
             bally += ballv*deltaTime;
-            ballx += ballvx;
-            System.out.println(bally + " ");
+            if (bally < 0) {
+                ballv = -ballv*0.9;
+                bally = ballv*deltaTime;
+                ballvx = ballv*Math.sin(Math.PI/6);
+            }
+            ballx += ballvx*deltaTime;
+            System.out.println(ballx + " ");
         }
     }
 }
