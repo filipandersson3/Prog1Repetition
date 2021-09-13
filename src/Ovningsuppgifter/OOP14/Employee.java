dd4847b6ec5d456eca7a47d6372a9140ac9a8ab5
filipandersson3 <60388955+filipandersson3@users.noreply.github.com>
2021-09-13 09:39:18 +0200

    gjort uppgift OOP 1.2 och börjat 1.4

---
+++ src/Ovningsuppgifter/OOP14/Employee.java
@@ -0,0 +1,15 @@
+package Ovningsuppgifter.OOP14;
+
+public class Employee {
+    private int id;
+    private String firstName;
+    private String lastName;
+    private int salary;
+
+    public Employee(int id, String firstName, String lastName, int salary) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.salary = salary;
+    }
+}
---
+++ src/Ovningsuppgifter/OOP14/TestMain.java
@@ -0,0 +1,2 @@
+package Ovningsuppgifter.OOP14;
+
